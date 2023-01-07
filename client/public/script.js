b4ed10a680eea81d09d483e9a0de19871925a060
magdalenahuget <magdalena.huget@gmail.com>
2023-01-07 19:05:49 +0100

    Add solution of the task 2.

--- client/public/script.js
+++ client/public/script.js
@@ -7,32 +7,101 @@ import { fares } from '/data.js';
 
 
 
+// const fareElement = function (departureAirport, arrivalAirport, arrivalDate, price, currency) {
+//   return `<h2>
+//       ${departureAirport} --> ${arrivalAirport} 
+//       ${arrivalDate} 
+//       ${price} ${currency}
+//     </h2>`;
+// }
+
+// const loadEvent = function () {
+
+
+//   const root = document.getElementById("root");
+//   console.log(fares);
+
+
+//   fares.forEach((fare) => {
+//     const outbound = fare.outbound;
+//     const h2FareElement = fareElement (
+//       outbound.departureAirport.name,
+//       outbound.arrivalAirport.name,
+//       outbound.arrivalDate,
+//       outbound.price.value,
+//       outbound.price.currencyCode
+//     );
+//     root.insertAdjacentHTML("beforeend", h2FareElement);
+//   })
+
+
+// }
+
+// window.addEventListener("load", loadEvent);
+
+
+
+
+
+// TASK 2 - Find the first cheapest fare - return the departure, arrival airport and it's price in an object.
+
+
+
 const fareElement = function (departureAirport, arrivalAirport, arrivalDate, price, currency) {
-  return `<h2>
+    return `<h2>
       ${departureAirport} --> ${arrivalAirport} 
       ${arrivalDate} 
       ${price} ${currency}
     </h2>`;
 }
 
+const fareElementTheCheapest = function (content) {
+    console.log(content)
+    return `<h1><em>The cheapest fare:</em> ${content.outbound.departureAirport.name} - ${content.outbound.arrivalAirport.name} ${content.outbound.price.value} ${content.outbound.price.currencyCode}<em>!!!</em></h1>`
+}
+
+const taskNumber = (command) => {
+    return `<h4>Task ${command}</h4>`;
+}
+
 const loadEvent = function () {
 
 
-  const root = document.getElementById("root");
-  console.log(fares);
+    const root = document.getElementById("root");
+    console.log(fares);
+
+    root.insertAdjacentHTML("beforeend", taskNumber(1));
+
+    fares.forEach((fare) => {
+        const outbound = fare.outbound;
+        const h2FareElement = fareElement(
+            outbound.departureAirport.name,
+            outbound.arrivalAirport.name,
+            outbound.arrivalDate,
+            outbound.price.value,
+            outbound.price.currencyCode
+        );
+        root.insertAdjacentHTML("beforeend", h2FareElement);
 
 
-  fares.forEach((fare) => {
-    const outbound = fare.outbound;
-    const h2FareElement = fareElement (
-      outbound.departureAirport.name,
-      outbound.arrivalAirport.name,
-      outbound.arrivalDate,
-      outbound.price.value,
-      outbound.price.currencyCode
-    );
-    root.insertAdjacentHTML("beforeend", h2FareElement);
-  })
+    })
+
+    // root.insertAdjacentHTML("beforebegin", taskNumber(1));
+
+    const theCheapest = () => {
+
+        let cheapestFlight = fares[0]
+        for (let fare of fares) {
+            if (fare.outbound.price.value < cheapestFlight.outbound.price.value) {
+                cheapestFlight = fare;
+            }
+        }
+        return cheapestFlight;
+    }
+    root.insertAdjacentHTML("beforeend", "</br></br>");
+    root.insertAdjacentHTML("beforeend", taskNumber(2));
+    root.insertAdjacentHTML("beforeend", fareElementTheCheapest(theCheapest()));
+
 
 
 }
